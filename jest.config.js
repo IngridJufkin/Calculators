bb01c0effc3431c69f8cf72c98901221c15f5256
Ingrid Jufkin <Ingrid@Ingrids-MBP.lan>
2021-03-08 21:53:48 +0200

    first commit

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  preset: "@vue/cli-plugin-unit-jest", 
+  setupFiles: ["<rootDir>/tests/setup.js"] //võimaldab testidel töötada vuetify-s root kaustast/test ja võetakse setup.js faili configuratsioonis arvesse, käiakse see fail läbi ja alles siis hakatakse teste jooksutama
+};
