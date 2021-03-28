f06faae230d30b89f4b573234e9cbd595dbbb903
Kaido24 <mumm.meelis@gmail.com>
2021-03-28 20:39:32 +0300

    piechart and kmi route

---
+++ src/views/BodyMass.vue
@@ -0,0 +1,16 @@
+<template>
+  <BodyMass />
+</template>
+
+<script>
+// @ is an alias to /src
+
+import BodyMass from "@/components/BodyMass.vue";
+
+export default {
+  name: "bodymass",
+  components: {
+    BodyMass,
+  },
+};
+</script>
